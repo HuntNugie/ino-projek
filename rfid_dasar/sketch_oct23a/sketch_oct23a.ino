0b2a2bee0b10915bef56d11027d63365f7e6a243
Nugie kurniawan <nugiebinhar@gmail.com>
2025-12-07 12:19:19 +0700

    rfid_dasar

--- rfid_dasar/sketch_oct23a/sketch_oct23a.ino
+++ rfid_dasar/sketch_oct23a/sketch_oct23a.ino
@@ -1,14 +1,18 @@
+//untuk library dari mfrc522
 #include <MFRC522.h>
+//untuk komunikasi
 #include <SPI.h>
 #define SSPIN D1
 #define RSTPIN D3
 
+//instance class 
 MFRC522 mfrc522(SSPIN,RSTPIN);
 
 
 void setup(){
 
    Serial.begin(115200);
+//    untuk inisialisasi dan membuka komunikasi
    SPI.begin();
    mfrc522.PCD_Init();
    Serial.println("rfid siap maniez");
@@ -16,13 +20,16 @@ void setup(){
 }
 
 void loop(){
+//  cek jika tidak ada kartu dan tidak ada uid di kartu jangan tampilkan apa apa
   if(!mfrc522.PICC_IsNewCardPresent())return;
  if(!mfrc522.PICC_ReadCardSerial())return;
 
   String uid = "";
  
   Serial.print("UID : ");
+//  karna uid dalam rfid nya tuh mengandung byte nya berpisah pisah maka untuk mendapatkan seluruh uid nya kita harus mengugnakan looping
  for(byte i = 0;i<mfrc522.uid.size;i++){
+//  untuk mengambil per satu pecahan dari uidByte nya dan di ubah menjadi hexadesimal
   Serial.print(mfrc522.uid.uidByte[i], HEX);
   String temp = String(mfrc522.uid.uidByte[i], HEX);
   temp.toUpperCase();
@@ -34,6 +41,7 @@ void loop(){
 
   Serial.print(" hasil akhir:");
   Serial.println(uid);
+//  untuk menutup komunikasi rfidnya
  mfrc522.PICC_HaltA();
  
 }
